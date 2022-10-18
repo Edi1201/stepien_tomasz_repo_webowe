13408a4df858bcc8e639d4ec68b37087dfef9050
Edi1201 <77607534+Edi1201@users.noreply.github.com>
2022-10-18 11:30:27 +0200

    fdsdsds

---
+++ bootstrapprojekt/script.js
@@ -0,0 +1,25 @@
+$(function (){
+    $("#mainText").text("Volkswagen Golf - samochód osobowy klasy kompaktowej produkowany pod niemiecką marką Volkswagen od 1974 roku. Od 2019 roku produkowana jest ósma generacja modelu.");
+})
+var images = ["1.png","2.png","3.png"];
+$.each(images, function(index, name){
+    if(index == 0){
+        $(".carousel-inner").append("<div class='carousel-item active'><img src="+name+" class='d-block w-100' alt="+name+"><div class='carousel-caption d-none d-md-block'></div></div>")
+    }else{
+        $(".carousel-inner").append("<div class='carousel-item '><img src="+name+" class='d-block w-100' alt="+name+"><div class='carousel-caption d-none d-md-block'></div></div>")
+    }
+})
+$("#btnAuthor").click(function(){
+    $("#mainText").html("Autor: Tomasz Stępień <br> &nbsp;");
+
+})
+$("#btnMain").click(function(){
+    $("#mainText").text("Volkswagen Golf - samochód osobowy klasy kompaktowej produkowany pod niemiecką marką Volkswagen od 1974 roku. Od 2019 roku produkowana jest ósma generacja modelu.");
+})
+
+
+
+
+
+
+
